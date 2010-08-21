89bc595b19b9ae9eb1a3b6051617a07f7c2bad55
Eumir Gaspar <eumir.gaspar@devex.com>
2010-08-21 16:03:03 +0800

    added plugin examples

--- js/simple.js
+++ js/simple.js
@@ -1,15 +1,41 @@
 $(document).ready(function() {
+  
+  //Example 1: Simple declaration with default coordinates and zoom
   var map = $("#map").gmap3( {
     lat: 25.928407,
     lng: -79.727783,
     zoom: 7
   });
   
-  map.addMarkerByLatLng(25.928407, -79.727783, "test");
-  map.addMarkerByLatLng(25.928407, -80.727783, "pew");
+  //Example 2: Simple map with button to add a specific marker
+  var map2 = $('#map2').gmap3( {
+    lat: 25.928407,
+    lng: -79.727783,
+    zoom: 7
+  })
   
-  $('#clickme').click( function() {
-    map.removeMarker("test");
+  $('#add-marker').click( function() {
+    //marker name is optional but required if you want the delete functionality
+    map2.addMarkerByLatLng(25.928407, -79.727783, "my-marker");
+    return false;
+  })
+  
+  //Example 3: Simple map with add/remove marker functionality
+  var map3 = $('#map3').gmap3( {
+    lat: 25.928407,
+    lng: -79.727783,
+    zoom: 7
+  })
+  
+  $('#add-marker2').click( function() {
+    //marker name is optional but required if you want the delete functionality
+    map3.addMarkerByLatLng(25.928407, -79.727783, "my-marker");
+    return false;
+  })
+  
+  $('#remove-marker').click( function() {
+    map3.removeMarker("my-marker");
+    return false;
   })
   
 });
